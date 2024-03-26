9596c372c3c0cf200fb87f62f9040d79fe2b4717
LSCAR23 <laraoscar2310@gmail.com>
2024-03-25 18:35:43 -0600

    contacts

--- src/Pages/MainInfoPage/MainInfoPage.js
+++ src/Pages/MainInfoPage/MainInfoPage.js
@@ -1,21 +1,43 @@
 import React from "react";
-import peronImage from '../../images/person.png'
+import peronImage from '../../images/me.jpeg'
 import './MainInfoPage.css'
 import reactIcon from '../../images/reactImg.png'
 import flutterIcon from '../../images/flutterImg.png'
 import pythonIcon from '../../images/pythonImg.png'
 import firebaseIcon from '../../images/firebaseImg.png'
 import csharpIcon from '../../images/csharpImg.png'
-import { useState, useEffect } from 'react';
+import ClipboardJS from 'clipboard';
+import { useState, useEffect, useRef } from 'react';
 const MainInfoPage = () => {
+    const phone= '+506 83105790';
+    const phoneRef = useRef(null);
+    const correo = 'laraoscar2310@gmail.com';
+    useEffect(() => {
+        const clipboard = new ClipboardJS(phoneRef.current, {
+            text: () => phone
+        });
 
+        clipboard.on('success', function(e) {
+            alert('Número copiado al portapapeles');
+            e.clearSelection();
+        });
 
+        // Limpiar en desmontaje
+        return () => {
+            clipboard.destroy();
+        };
+    }, []);
 
     return (
         <div>
             <div className="firstInfoContent">
                 <div className="imageContent">
                     <img className="myImage" src={peronImage}></img>
+                    <div className="contactInfoContainer">
+                        <h3 className="name">I'm Oscar</h3>
+                        <a href={`mailto:${correo}`} className="correo">{correo}</a>
+                        <div className="phone" ref={phoneRef} >{phone}</div>                    
+                    </div>
                 </div>
 
                 <div className="aboutContent">
@@ -25,11 +47,11 @@ const MainInfoPage = () => {
             <div className="skillsContent">
                 <h2 className="skillsTitle">Tech Stack</h2>
                 <div className="iconContent">
-                <img className="imgIcon" style={{animation: "slideIcon1 1s ease-in-out 0s 1"}} src={reactIcon} />
-                <img className="imgIcon" style={{animation: "slideIcon2 1.5s ease-in-out 0s 1"}} src={flutterIcon} />
-                <img className="imgIcon" style={{animation: "slideIcon3 2s ease-in-out 0s 1"}} src={firebaseIcon} />
-                <img className="imgIcon" style={{animation: "slideIcon4 2.5s ease-in-out 0s 1"}} src={csharpIcon} />
-                <img className="imgIcon" style={{animation: "slideIcon5 3s ease-in-out 0s 1"}} src={pythonIcon} />
+                    <img className="imgIcon" style={{ animation: "slideIcon1 1s ease-in-out 0s 1" }} src={reactIcon} />
+                    <img className="imgIcon" style={{ animation: "slideIcon2 1.5s ease-in-out 0s 1" }} src={flutterIcon} />
+                    <img className="imgIcon" style={{ animation: "slideIcon3 2s ease-in-out 0s 1" }} src={firebaseIcon} />
+                    <img className="imgIcon" style={{ animation: "slideIcon4 2.5s ease-in-out 0s 1" }} src={csharpIcon} />
+                    <img className="imgIcon" style={{ animation: "slideIcon5 3s ease-in-out 0s 1" }} src={pythonIcon} />
 
                 </div>
             </div>

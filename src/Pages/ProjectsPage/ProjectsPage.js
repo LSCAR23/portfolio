1528b8dbed20e2c3d06e6a4d44efc7bf0893cc01
LSCAR23 <laraoscar2310@gmail.com>
2024-03-28 21:59:59 -0600

    project descriptions

--- src/Pages/ProjectsPage/ProjectsPage.js
+++ src/Pages/ProjectsPage/ProjectsPage.js
@@ -1,7 +1,6 @@
 import React from "react";
 import './ProjectsPage.css'
 import Card from "../../Components/Card/Card";
-import me from "../../images/me.jpeg";
 import jobsworkers from '../../images/jobsWorkers/jworkers1.png';
 import jobsworkers2 from '../../images/jobsWorkers/jworkers2.png';
 import jobsworkers3 from '../../images/jobsWorkers/jworkers3.png';
@@ -23,19 +22,19 @@ const ProjectsPage = () => {
         {
             title: 'JOBS',
             image: jobs,
-            description: "I'm a passionate software engineering student nearing completion of my diploma, eager to translate my learnings into real-world applications.  I possess a strong foundation in various technologies like React, Python, Firebase, C#, PostgreSQL, Flutter, and Java, honed through university projects and personal endeavors.  My drive to learn extends beyond programming, as I'm also finalizing my English studies at the university.  I thrive in environments that encourage continuous learning and innovation, allowing me to explore the potential of different technologies and create impactful solutions.",
+            description: "A platform that bridges the gap between workers seeking casual jobs and clients who need their services. The project, built using Flutter, leverages Firebase as its backend database. As a full-stack developer, I’ve handled all aspects of development so far.",
             images:[jobs,jobs2,jobs3,jobs4,jobs5]
         },
         {
             title: 'JOBS WORKERS',
             image: jobsworkers,
-            description: "I'm a passionate software engineering student nearing completion of my diploma, eager to translate my learnings into real-world applications.  I possess a strong foundation in various technologies like React, Python, Firebase, C#, PostgreSQL, Flutter, and Java, honed through university projects and personal endeavors.  My drive to learn extends beyond programming, as I'm also finalizing my English studies at the university.  I thrive in environments that encourage continuous learning and innovation, allowing me to explore the potential of different technologies and create impactful solutions.",
+            description: "I’m currently in the process of developing a comprehensive platform that caters to casual workers seeking employment opportunities. This application serves as the ideal bridge between these workers and potential clients. Leveraging the power of Flutter, I’m crafting an intuitive and user-friendly interface that simplifies job discovery for workers and streamlines the hiring process for clients. The project shares a common Firebase database with JOBS, ensuring seamless data synchronization and efficient management. As a dedicated full-stack developer, I’ve taken charge of every aspect of this project, from conceptualization to implementation.",
             images:[jobsworkers,jobsworkers2,jobsworkers3,jobsworkers4]
         },
         {
             title: 'WEB SITE MATEORITE AGUAS ZARCAS',
             image: pagMeteorito1,
-            description: "I'm a passionate software engineering student nearing completion of my diploma, eager to translate my learnings into real-world applications.  I possess a strong foundation in various technologies like React, Python, Firebase, C#, PostgreSQL, Flutter, and Java, honed through university projects and personal endeavors.  My drive to learn extends beyond programming, as I'm also finalizing my English studies at the university.  I thrive in environments that encourage continuous learning and innovation, allowing me to explore the potential of different technologies and create impactful solutions.",
+            description: "A simple website for a university community, in which I collaborate in its development. This site, built using React, focuses on a meteorite that fell near the community of Aguas Zarcas. It will include scientific data, curiosities and information about both the local community and the nearby areas impacted by the meteorite.",
             images:[pagMeteorito1,pagMeteorito2,pagMeteorito3,pagMeteorito4,pagMeteorito5]
         }
     ];
